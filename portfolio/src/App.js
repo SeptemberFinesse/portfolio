22dc68b63c892694f5a6d32adb1b5ff388e33ae4
SeptemberFinesse <finessefilmproductions@gmail.com>
2024-08-31 22:55:55 -0400

    attempted

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import Section from './components/Section';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WeatherApp from './components/weather-finesse-app/src/App';
+import StockSearchApp from './components/market-data-app/src/App';
+import { Link } from 'react-router-dom';
+
 import './App.css';
 
 const App = () => {
+
   const [clickedNav, setClickedNav] = useState('');
   const [hoveredSection, setHoveredSection] = useState('');
   const [showText, setShowText] = useState(false); // State for text fade-in
@@ -38,61 +44,70 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <div className="my-header">
-        <h1 onClick={handleHeaderClick}>LORENZO LLAMAS</h1>
-        <nav>
-          <div className="about" onClick={() => handleNavClick('about')}>
-            ABOUT
+    
+    <Router>
+      <Routes>
+      <div className="App">
+          <div className="my-header">
+            <h1 onClick={handleHeaderClick}>LORENZO LLAMAS</h1>
+            <nav>
+              <div className="about" onClick={() => handleNavClick('about')}>
+                ABOUT
+              </div>
+              <div className="contact">
+                CONTACT
+              </div>
+            </nav>
           </div>
-          <div className="contact">
-            CONTACT
+          <div className="sections">
+            <Route path="/portfolio/weather" element={<WeatherApp />} />
+            <li><Link to="/portfolio/weather">Weather App</Link></li>
+              <Section
+                content={clickedNav === 'about' ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' : ''}
+                gif="https://media.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif"
+                isNavClicked={clickedNav === 'about'}
+                showText={showText}
+                isHovered={hoveredSection === 'section1'}
+                flashAnimation={flashAnimation}
+                onMouseEnter={() => handleSectionHover('section1')}
+                onMouseLeave={handleSectionLeave}
+              />
+            <Route path="/portfolio/stocksearch" element={<StockSearchApp />} />  
+              <Section
+                content={clickedNav === 'about' ? 'Lorenzo Llamas (Developer, Technical Consultant, Client Relationship Manager)' : ''}
+                gif="https://media.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif"
+                isNavClicked={clickedNav === 'about'}
+                showText={showText}
+                isHovered={hoveredSection === 'section2'}
+                flashAnimation={flashAnimation}
+                onMouseEnter={() => handleSectionHover('section2')}
+                onMouseLeave={handleSectionLeave}
+              />
+            <Section
+              content=""
+              gif="https://media.giphy.com/media/L1R1tvI9svkIWwpVYr/giphy.gif"
+              isNavClicked={clickedNav === 'about'}
+              showText={showText}
+              isHovered={hoveredSection === 'section3'}
+              flashAnimation={flashAnimation}
+              onMouseEnter={() => handleSectionHover('section3')}
+              onMouseLeave={handleSectionLeave}
+            />
+            <Section
+              content={clickedNav === 'about' ? 'Instagram: FinesseCoding YouTube: FinesseCoding' : ''}
+              gif="https://gifdb.com/images/high/coding-animated-laptop-flow-stream-ja04010rm5o68zfk.gif"
+              isNavClicked={clickedNav === 'about'}
+              showText={showText}
+              isHovered={hoveredSection === 'section4'}
+              flashAnimation={flashAnimation}
+              onMouseEnter={() => handleSectionHover('section4')}
+              onMouseLeave={handleSectionLeave}
+            />
           </div>
-        </nav>
-      </div>
-      <div className="sections">
-        <Section
-          content={clickedNav === 'about' ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' : ''}
-          gif="https://media.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif"
-          isNavClicked={clickedNav === 'about'}
-          showText={showText}
-          isHovered={hoveredSection === 'section1'}
-          flashAnimation={flashAnimation}
-          onMouseEnter={() => handleSectionHover('section1')}
-          onMouseLeave={handleSectionLeave}
-        />
-        <Section
-          content={clickedNav === 'about' ? 'Lorenzo Llamas (Developer, Technical Consultant, Client Relationship Manager)' : ''}
-          gif="https://media.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif"
-          isNavClicked={clickedNav === 'about'}
-          showText={showText}
-          isHovered={hoveredSection === 'section2'}
-          flashAnimation={flashAnimation}
-          onMouseEnter={() => handleSectionHover('section2')}
-          onMouseLeave={handleSectionLeave}
-        />
-        <Section
-          content=""
-          gif="https://media.giphy.com/media/L1R1tvI9svkIWwpVYr/giphy.gif"
-          isNavClicked={clickedNav === 'about'}
-          showText={showText}
-          isHovered={hoveredSection === 'section3'}
-          flashAnimation={flashAnimation}
-          onMouseEnter={() => handleSectionHover('section3')}
-          onMouseLeave={handleSectionLeave}
-        />
-        <Section
-          content={clickedNav === 'about' ? 'Instagram: FinesseCoding YouTube: FinesseCoding' : ''}
-          gif="https://gifdb.com/images/high/coding-animated-laptop-flow-stream-ja04010rm5o68zfk.gif"
-          isNavClicked={clickedNav === 'about'}
-          showText={showText}
-          isHovered={hoveredSection === 'section4'}
-          flashAnimation={flashAnimation}
-          onMouseEnter={() => handleSectionHover('section4')}
-          onMouseLeave={handleSectionLeave}
-        />
-      </div>
-    </div>
+        </div>
+      </Routes>
+    </Router>
+    
   );
 };
 
--- portfolio/src/components/weather-finesse-app/src/App.js
+++ portfolio/src/components/weather-finesse-app/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Section from '../../weather-finesse-app/src/components/Weather';
+
 import Weather from './components/Weather';
 
 const App = () => {
@@ -7,6 +9,16 @@ const App = () => {
     <div>
       <Outlet />
       {/* <Weather /> */}
+      <Section
+                content={clickedNav === 'about' ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' : ''}
+                gif="https://media.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif"
+                isNavClicked={clickedNav === 'about'}
+                showText={showText}
+                isHovered={hoveredSection === 'section1'}
+                flashAnimation={flashAnimation}
+                onMouseEnter={() => handleSectionHover('section1')}
+                onMouseLeave={handleSectionLeave}
+              />
     </div>
   );
 };
